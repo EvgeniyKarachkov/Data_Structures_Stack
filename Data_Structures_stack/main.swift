66e2922945182f49a5acfe3e5dce326f0c9b553b
EvgeniyKarachkov <cazinak341@gmail.com>
2021-04-09 17:06:37 +0300

    Start Work

---
+++ Data_Structures_stack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Data_Structures_stack
+//
+//  Created by Zodino BLR on 4/9/21.
+//  Copyright © 2021 Zodino BLR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
